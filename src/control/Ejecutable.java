eb1748fef927018b7a1143f661fee45e6d4a6601
Daniel Steven Ocampo <danielsteven23@outlook.com>
2018-11-30 12:30:29 -0500

    my second commit

---
+++ src/control/Ejecutable.java
@@ -0,0 +1,34 @@
+package control;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejecutable {
+
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+		
+			System.out.println("Calculadora");
+			System.out.println("Valor A:");
+			String linea = in.readLine();
+			int A = Integer.parseInt(linea);
+			
+			System.out.println("Valor B:");
+			linea = in.readLine();
+			int B= Integer.parseInt(linea);
+			System.out.println("A+B= "+ sumar(A,B));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static int sumar (int a,int b) {		
+		int resultado = a + b;
+		return resultado;
+	}
+
+}
---
+++ src/test/TestCases.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import control.Ejecutable;
+
+public class TestCases {
+
+	@Test
+	public void testSuma(){
+		double ram = Math.random() * 100;
+		
+		int ramInt = (int) ram; 
+		
+		double ram1 = Math.random() * 100;
+		
+		int ramInt2 = (int) ram1; 
+		
+		int resultado = ramInt + ramInt2;
+		
+		int resultadoCalculadora = Ejecutable.sumar(ramInt, ramInt2);
+		
+		assertEquals(resultado, resultadoCalculadora);
+	}
+}
